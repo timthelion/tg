78db5ecf428801d8eb452ebb9d922b19812f45f2
Timothy <timothyhobbs@seznam.cz>
2016-01-30 18:35:44 +0100

    Lots more work

--- editors-and-viewers/tg.py
+++ editors-and-viewers/tg.py
@@ -46,27 +46,24 @@ class TextGraph(list):
     with open(self.fileText,"w") as fd:
       fd.write(serialized)
 
-class GraphView():
+class GraphView(urwid.Pile):
   def __init__(self,graph):
     self.graph = graph
-    self.selection = 0
-    self.pallet = [('backlink_selected', 'white', 'dark blue')
-                  ,('link_selected', 'white', 'dark red')
-                  ,('selection','black','white')
-                  ,('clipboard','white','dark gray')]
+    self._selection = 0
+    self.history = []
     # clipboard
     self.clipboard = Clipboard(self)
     # backlinks
     self.backlinks = BackLinksList(self)
     # current node
     self.currentNode = urwid.Edit(edit_text="",align="center",multiline=True)
-    self.currentNodeWidget = urwid.AttrMap(self.currentNode,None,"selection")
+    self.currentNodeWidget = urwid.Filler(urwid.AttrMap(self.currentNode,None,"selection"))
     # links
     self.links = LinksList(self)
     # command bar
     self.commandBar = CommandBar(self)
     # main pile
-    self.mainPile = urwid.Pile([self.clipboard,self.backlinks,urwid.Filler(self.currentNodeWidget),self.links,self.commandBar])
+    super(GraphView,self).__init__([self.clipboard,self.backlinks,self.currentNodeWidget,self.links,self.commandBar])
     self.update()
 
   def update(self):
@@ -82,8 +79,26 @@ class GraphView():
       nodes.append(self.graph[nodeId])
     self.links.update(nodes)
 
-  def show(self):
-    urwid.MainLoop(self.mainPile,self.pallet).run()
+  @property
+  def selection(self):
+    return self._selection
+
+  @selection.setter
+  def selection(self,value):
+    self.history.append(self.selection)
+    self._selection = value
+
+  def keypress(self,size,key):
+    if key in keybindings["back"]:
+      if self.history:
+        self._selection = self.history.pop()
+        self.update()
+    elif key in keybindings['jump-to-command-bar'] and self.focus_item != self.currentNodeWidget:
+      self.focus_item = self.commandBar
+    elif key in keybindings['jump-to-node-edit-box']:
+      self.focus_item = self.currentNodeWidget
+    else:
+      return super(GraphView,self).keypress(size,key)
 
 class NodeList(urwid.ListBox):
   def __init__(self,selectionCollor,alignment):
@@ -93,11 +108,14 @@ class NodeList(urwid.ListBox):
     super(NodeList,self).__init__(urwid.SimpleFocusListWalker([]))
 
   def update(self,nodes=None):
-    if nodes:
+    if nodes is not None:
       self.nodes = nodes
     items = []
     for node in self.nodes:
-      title = node.text.splitlines()[0]
+      try:
+        title = node.text.splitlines()[0]
+      except IndexError:
+        title = "<blank-text>"
       items.append(urwid.AttrMap(urwid.Padding(urwid.SelectableIcon(title,0),align=self.alignment,width="pack"),None,self.selectionCollor))
     self.body.clear()
     self.body.extend(items)
@@ -108,26 +126,36 @@ class Clipboard(NodeList):
     super(Clipboard,self).__init__("clipboard","right")
 
   def keypress(self,size,key):
-    if key == 'd':
+    if key in keybindings["remove-from-stack"]:
       fcp = self.focus_position
       del self.nodes[fcp]
       self.update()
       if fcp < len(self.nodes):
         self.focus_position = fcp
-    if key == 'right' or key == 'ctrl right':
-      node = self.nodes[self.focus_position]
-      if not key == 'ctrl right':
-        del self.nodes[self.focus_position]
-      self.view.graph[self.view.selection].links.append(node.nodeId)
-      self.view.graph.edited = True
-      self.view.update()
-    if key == 'left' or key == 'ctrl left':
-      node = self.nodes[self.focus_position]
-      if not key == 'ctrl left':
-        del self.nodes[self.focus_position]
-      node.links.append(self.view.selection)
-      self.view.graph.edited = True
-      self.view.update()
+    if key in keybindings['link-to-stack-item'] or key in keybindings['link-to-stack-item-no-pop']:
+      try:
+        fcp = self.focus_position
+      except IndexError:
+        pass
+      else:
+        node = self.nodes[fcp]
+        if not key in keybindings['link-to-stack-item-no-pop']:
+          del self.nodes[fcp]
+        self.view.graph[self.view.selection].links.append(node.nodeId)
+        self.view.graph.edited = True
+        self.view.update()
+    if key in keybindings['backlink-to-stack-item'] or key in keybindings['backlink-to-stack-item-no-pop']:
+      try:
+        fcp = self.focus_position
+      except IndexError:
+        pass
+      else:
+        node = self.nodes[fcp]
+        if not key in keybindings['backlink-to-stack-item-no-pop']:
+          del self.nodes[fcp]
+        node.links.append(self.view.selection)
+        self.view.graph.edited = True
+        self.view.update()
     else:
       return super(Clipboard,self).keypress(size,key)
 
@@ -137,16 +165,19 @@ class NodeNavigator(NodeList):
     super(NodeNavigator,self).__init__(selectionCollor,alignment)
 
   def keypress(self,size,key):
-    if key == self.alignment:
-      newText = self.view.currentNode.edit_text
-      if self.view.graph[self.view.selection].text != newText:
-        self.view.graph[self.view.selection].text = newText
-        self.view.graph.edited = True
-      self.view.selection = self.nodes[self.focus_position].nodeId
-      self.view.update()
-    if key == 'c':
+    if key in [self.alignment,'enter']:
+      if self.nodes:
+        newText = self.view.currentNode.edit_text
+        if self.view.graph[self.view.selection].text != newText:
+          self.view.graph[self.view.selection].text = newText
+          self.view.graph.edited = True
+        self.view.selection = self.nodes[self.focus_position].nodeId
+        self.view.update()
+    if key in keybindings["add-to-stack"]:
       self.view.clipboard.nodes.append(self.nodes[self.focus_position])
+      fcp = self.focus_position
       self.view.update()
+      self.focus_position = fcp
     else:
       return super(NodeNavigator,self).keypress(size,key)
 
@@ -155,13 +186,34 @@ class BackLinksList(NodeNavigator):
     self.view = view
     super(BackLinksList,self).__init__(view,'backlink_selected','left')
 
+  def keypress(self,size,key):
+    if key in keybindings['new-node']:
+      nodeId = len(self.view.graph)
+      node = Node(nodeId,"",[self.view.selection])
+      self.view.graph.append(node)
+      self.view.selection = nodeId
+      self.view.update()
+      self.view.focus_item = self.view.currentNodeWidget
+    if key in ['right']:
+      self.view.focus_item = self.view.links
+      self.view.links.focus_position = 0
+    if key in keybindings['link-or-back-link-last-stack-item']:
+      if self.view.clipboard.nodes:
+        node = self.view.clipboard.nodes.pop()
+        node.links.append(self.view.selection)
+        self.view.graph.edited = True
+        self.view.update()
+        self.focus_position = self.nodes.index(node)
+    else:
+      return super(BackLinksList,self).keypress(size,key)
+
 class LinksList(NodeNavigator):
   def __init__(self,view):
     self.view = view
     super(LinksList,self).__init__(view,'link_selected','right')
 
   def keypress(self,size,key):
-    if key == 'ctrl up':
+    if key in keybindings['move-node-up']:
       sel = self.view.graph[self.view.selection]
       fcp = self.focus_position
       if fcp >= 0:
@@ -172,7 +224,7 @@ class LinksList(NodeNavigator):
         self.view.graph.edited = True
         self.view.update()
         self.focus_position = fcp - 1
-    elif key == 'ctrl down':
+    elif key in keybindings['move-node-down']:
       sel = self.view.graph[self.view.selection]
       fcp = self.focus_position
       if fcp < len(sel.links):
@@ -183,9 +235,29 @@ class LinksList(NodeNavigator):
         self.view.graph.edited = True
         self.view.update()
         self.focus_position = fcp + 1
+    elif key in keybindings['new-node']:
+      nodeId = len(self.view.graph)
+      node = Node(nodeId,"",[])
+      self.view.graph.append(node)
+      self.view.graph[self.view.selection].links.append(nodeId)
+      self.view.selection = nodeId
+      self.view.update()
+      self.view.focus_item = self.view.currentNodeWidget
+    if key in ['left']:
+      self.view.focus_item = self.view.backlinks
+    if key in keybindings['link-or-back-link-last-stack-item']:
+      if self.view.clipboard.nodes:
+        try:
+          fcp = self.focus_position
+        except IndexError:
+          fcp = -1
+        node = self.view.clipboard.nodes.pop()
+        self.view.graph[self.view.selection].links.insert(fcp + 1,node.nodeId)
+        self.view.graph.edited = True
+        self.view.update()
+        self.focus_position = fcp + 1
     else:
       return super(LinksList,self).keypress(size,key)
-
 
 class CommandBar(urwid.Filler):
   def __init__(self,view):
@@ -214,4 +286,24 @@ if not len(args) == 1:
   sys.exit("tg expects to be passed a single file path for editing. Use --help for help.")
 
 graphView = GraphView(TextGraph(args[0]))
-graphView.show()
+keybindings = {
+ 'back' : ['meta left','b'],
+ 'remove-from-stack' : ['d','delete'],
+ 'link-to-stack-item-no-pop' : ['ctrl right'],
+ 'link-to-stack-item' : ['right'],
+ 'backlink-to-stack-item-no-pop' : ['ctrl left'],
+ 'backlink-to-stack-item' : ['left'],
+ 'link-or-back-link-last-stack-item' : ['p'],
+ 'add-to-stack' : ['c'],
+ 'move-node-up' : ['ctrl up'],
+ 'move-node-down' : ['ctrl down'],
+ 'new-node' : ['n'],
+ 'jump-to-command-bar' : [':'],
+ 'jump-to-node-edit-box' : ['esc'],
+ }
+pallet = [('backlink_selected', 'white', 'dark blue')
+         ,('link_selected', 'white', 'dark red')
+         ,('selection','black','white')
+         ,('clipboard','white','dark gray')]
+
+urwid.MainLoop(graphView,pallet).run()
